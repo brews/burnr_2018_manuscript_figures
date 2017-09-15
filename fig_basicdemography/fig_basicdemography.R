6ebb8a7f481af403b953dcfee2bcdbce3541a212
Brewster Malevich <sbmalev@gmail.com>
2017-09-15 11:51:41 -0700

    Broke demography fig in two

---
+++ fig_basicdemography/fig_basicdemography.R
@@ -0,0 +1,8 @@
+require(burnr)
+
+data(lgr2)
+
+
+svg("fig_basicdemography.svg", width = 5.5, height = 3.5)
+plot(lgr2)
+dev.off()
--- fig_colordemography/fig_colordemography.R
+++ fig_colordemography/fig_colordemography.R
@@ -4,12 +4,7 @@ data(lgr2)
 data(lgr2_meta)
 
 
-svg("fig_demography_a.svg", width = 6.5, height = 3)
-plot(lgr2)
-dev.off()
-
-
-svg("fig_demography_b.svg", width = 6.5, height = 2.75)
+svg("fig_colordemography_a.svg", width = 5.5, height = 3.5)
 plot(lgr2, ylabels = FALSE,
      color_group = lgr2_meta$SpeciesID,
      color_id = lgr2_meta$TreeID, 
@@ -19,7 +14,7 @@ dev.off()
 
 library(ggplot2)
 
-svg("fig_demography_c.svg", width = 6.5, height = 2.75)
+svg("fig_colordemography_b.svg", width = 5.5, height = 3.5)
 p <- plot_demograph(sort(lgr2, decreasing=TRUE),
              color_group = lgr2_meta$SpeciesID, 
              color_id = lgr2_meta$TreeID, 
@@ -29,7 +24,7 @@ p <- (p + annotate('rect', alpha = 0.2,
                    xmin = 1861, xmax = 1880,
                    ymin = 2.5, ymax = 15.5)
         # + guides(col = guide_legend(ncol = 2))
-        + theme(legend.position = c(0.25, 0.4), 
+        + theme(legend.position = c(0.27, 0.4), 
                 legend.direction = 'horizontal'))
 print(p)
 dev.off()
