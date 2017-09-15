7c11813e873a97ed93eb96de1b8598cd59c3769b
Brewster Malevich <sbmalev@gmail.com>
2017-09-15 12:03:52 -0700

    Rename and adjust height for Bando intervals figs

--- fig_bandointervals/fig_bandointervals.R
+++ fig_bandointervals/fig_bandointervals.R
@@ -1,11 +1,14 @@
 require(burnr)
 
+
 data(pgm)
+
 
 pgm_comp <- composite(pgm, comp_name = "PGM")
 pgm_interv <- intervals(pgm_comp)
 
-svg("fig3.svg", width = 6.5, height = 4, )
+
+svg("fig_bandointervals.svg", width = 5.5, height = 3)
 par(mfrow = c(1, 2))
 lab <- "Fire interval (yr)"
 hist(pgm_interv$intervals, xlab = lab, main = "")
